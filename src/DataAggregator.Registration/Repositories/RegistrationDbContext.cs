d3cff692a918a7f57fffc65c606a5c81661df3ef
CoJaques <colin.jaques@heig-vd.ch>
2025-05-25 18:06:39 +0200

    feat: simplify db configuration

--- src/DataAggregator.Registration/Repositories/RegistrationDbContext.cs
+++ src/DataAggregator.Registration/Repositories/RegistrationDbContext.cs
@@ -27,27 +27,20 @@ public class RegistrationDbContext : DbContext
     {
         base.OnModelCreating(builder);
 
-        // Configure Device entity
+        // Configure relationships for Device entity
         builder.Entity<Device>(entity =>
         {
             entity.HasKey(d => d.DeviceId);
-            entity.Property(d => d.DeviceName).IsRequired().HasMaxLength(100);
-            entity.Property(d => d.Location).HasMaxLength(200);
-            entity.Property(d => d.HealthCheckEndpoint).HasMaxLength(200);
-            entity.Property(d => d.AssignedTimeSeriesEndpoint).HasMaxLength(200);
             entity.HasMany(d => d.Sensors)
                   .WithOne(s => s.Device)
                   .HasForeignKey(s => s.DeviceId)
                   .OnDelete(DeleteBehavior.Cascade);
         });
 
-        // Configure Sensor entity
+        // Configure relationships for Sensor entity
         builder.Entity<Sensor>(entity =>
         {
             entity.HasKey(s => s.SensorId);
-            entity.Property(s => s.SensorName).IsRequired().HasMaxLength(100);
-            entity.Property(s => s.SensorType).HasMaxLength(50);
-            entity.Property(s => s.Unit).HasMaxLength(50);
         });
     }
 }
