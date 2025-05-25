49fc0eaa2fe5fccbc7d5ef1841a7b3af556d8e95
CoJaques <colin.jaques@heig-vd.ch>
2025-05-25 18:40:19 +0200

    feat: implement influx endpoints management

--- src/DataAggregator.Registration/Program.cs
+++ src/DataAggregator.Registration/Program.cs
@@ -1,8 +1,13 @@
+using DataAggregator.Shared;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Bind InfluxEndpoints configuration
+builder.Services.Configure<List<InfluxEndpointConfiguration>>(builder.Configuration.GetSection("InfluxEndpoints"));
 
 WebApplication app = builder.Build();
 
--- src/DataAggregator.Registration/Services/DeviceRegistrationService.cs
+++ src/DataAggregator.Registration/Services/DeviceRegistrationService.cs
@@ -1,14 +1,21 @@
 using DataAggregator.Registration.Repositories;
 using DataAggregator.Shared;
+using Microsoft.Extensions.Options;
 
 namespace DataAggregator.Registration.Services;
 
 /// <summary>
 /// Class for handling device registration operations.
 /// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="DeviceRegistrationService"/> class.
+/// </remarks>
 /// <param name="deviceRepository">The device repository.</param>
-public class DeviceRegistrationService(IDeviceRepository deviceRepository) : IDeviceRegistrationService
+/// <param name="influxEndpoints">The list of InfluxDB endpoint configurations.</param>
+public class DeviceRegistrationService(IDeviceRepository deviceRepository, IOptions<List<InfluxEndpointConfiguration>> influxEndpoints) : IDeviceRegistrationService
 {
+    private readonly List<InfluxEndpointConfiguration> _influxEndpoints = influxEndpoints.Value;
+
     /// <inheritdoc/>
     public Task<DeviceRegistrationResponse> RegisterCollectorAsync(DeviceRegistrationRequest request) => throw new NotImplementedException();
 
@@ -21,5 +28,16 @@ public class DeviceRegistrationService(IDeviceRepository deviceRepository) : IDe
     /// <inheritdoc/>
     public Task<IEnumerable<CollectorInfoDto>> GetAllCollectorInfoAsync() => throw new NotImplementedException();
 
-    private Task<InfluxEndpointConfiguration> GetAvailableInfluxEndpoint() => throw new NotImplementedException();
+    /// <summary>
+    /// Gets the available InfluxDB endpoint configuration.
+    /// This method can be enhanced to define a specific logic for selecting the endpoint.
+    /// </summary>
+    /// <returns>The default InfluxDB endpoint configuration.</returns>
+    public Task<InfluxEndpointConfiguration> GetAvailableInfluxEndpoint()
+    {
+        InfluxEndpointConfiguration? defaultEndpoint = _influxEndpoints.FirstOrDefault(e => e.IsDefault);
+        return defaultEndpoint == null
+            ? throw new InvalidOperationException("No default InfluxDB endpoint is configured.")
+            : Task.FromResult(defaultEndpoint);
+    }
 }
