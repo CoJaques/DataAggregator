b6f579b2226b13c747aab8fa51fbfed92722bddc
CoJaques <colin.jaques@heig-vd.ch>
2025-05-29 15:29:37 +0200

    feat: add history to db

--- src/DataAggregator.Registration/DeviceManagement/Persistence/Configuration/SensorConfiguration.cs
+++ src/DataAggregator.Registration/DeviceManagement/Persistence/Configuration/SensorConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using DataAggregator.Registration.DeviceManagement.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace DataAggregator.Registration.DeviceManagement.Persistence.Configuration;
@@ -20,11 +21,18 @@ public class SensorConfiguration : IEntityTypeConfiguration<Sensor>
         builder.Property(s => s.SensorType).HasMaxLength(50);
         builder.Property(s => s.Unit).HasMaxLength(50);
 
+        // Serialize Metadata as JSON with ValueComparer
+        var metadataComparer = new ValueComparer<Dictionary<string, string>>(
+            (d1, d2) => d1!.SequenceEqual(d2!),
+            d => d.Aggregate(0, (a, v) => HashCode.Combine(a, v.Key.GetHashCode(), v.Value.GetHashCode())),
+            d => d.ToDictionary(entry => entry.Key, entry => entry.Value));
+
         builder.Property(s => s.Metadata)
             .HasConversion(
                 v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
                 v => DeserializeMetadata(v))
-            .HasColumnType("json"); // TODO -> Test with Jsonb
+            .HasColumnType("json")
+            .Metadata.SetValueComparer(metadataComparer);
     }
 
     private static Dictionary<string, string> DeserializeMetadata(string json)
