47300d9bc601ca338fe16e4c09ed706a9fc41ec5
CoJaques <colin.jaques@heig-vd.ch>
2025-05-30 17:39:59 +0200

    feat: clean measurementData

--- src/DataAggregator.Collector/DataCollector/Models/MeasurementData.cs
+++ src/DataAggregator.Collector/DataCollector/Models/MeasurementData.cs
@@ -1,33 +1,6 @@
 namespace DataAggregator.Collector.DataCollector.Models;
 
 /// <summary>
-/// Interface for common measurement data operations regardless of the value type.
-/// </summary>
-public interface IMeasurementData
-{
-    /// <summary>
-    /// Gets the timestamp when the measurement was taken.
-    /// </summary>
-    DateTime TimeStamp { get; }
-    
-    /// <summary>
-    /// Gets the name of the sensor that produced this measurement.
-    /// </summary>
-    string SensorName { get; }
-    
-    /// <summary>
-    /// Gets the type of the measurement value.
-    /// </summary>
-    Type ValueType { get; }
-    
-    /// <summary>
-    /// Gets the raw value of the measurement as an object.
-    /// </summary>
-    /// <returns>The measurement value as an object.</returns>
-    object GetRawValue();
-}
-
-/// <summary>
 /// Represents a single measurement data point from a sensor.
 /// </summary>
 /// <typeparam name="T">The type of the measurement value.</typeparam>
@@ -35,7 +8,7 @@ public record MeasurementData<T>(DateTime TimeStamp, string SensorName, T Value)
 {
     /// <inheritdoc/>
     public Type ValueType => typeof(T);
-    
+
     /// <inheritdoc/>
     public object GetRawValue() => Value!;
 }
