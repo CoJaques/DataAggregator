fa34716e4b55ad0ac3847e4ea8e93bbf1648dde1
CoJaques <colin.jaques@heig-vd.ch>
2025-07-18 12:59:55 +0200

    feat: create interface for MachinePredictionProcessor

---
+++ src/DataAggregator.Processor/Services/Prediction/IMachinePredictionProcessor.cs
@@ -0,0 +1,16 @@
+﻿using DataAggregator.Processor.Configuration;
+
+namespace DataAggregator.Processor.Services.Prediction;
+
+/// <summary>
+/// Interface for processing machine predictions.
+/// </summary>
+public interface IMachinePredictionProcessor
+{
+    /// <summary>
+    /// Processes prediction for a specific machine.
+    /// </summary>
+    /// <param name="config">The machine prediction configuration.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public Task ProcessAsync(MachinePredictionConfig config);
+}
